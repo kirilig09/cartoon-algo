c4cba487540fb0ad418ecaacdafef54fb2a7e239
simo1209 <simeongeorgiev09@gmail.com>
2018-03-17 19:50:55 +0200

    Judge http endpoint works

--- src/main/java/com/example/controllers/AlgorithmController.java
+++ src/main/java/com/example/controllers/AlgorithmController.java
@@ -5,22 +5,25 @@ import com.example.services.AlgorithmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AlgorithmController {
     @Autowired
     AlgorithmService algorithmService;
 
-    @RequestMapping("/api/algos")
+    @GetMapping("/api/algos")
     public Page<Algorithm> getAlgorithms(Pageable pageable){
         return algorithmService.listAllByPage(pageable);
     }
 
-    @RequestMapping("/api/algos/:id")
+    @GetMapping("/api/algos/:id")
     public Algorithm algorithmById(@PathVariable long id){
         return algorithmService.findById(id);
+    }
+
+    @PostMapping("/judge")
+    public String judgeSystem(){//@RequestBody String body){
+        return "judge";
     }
 }
